d00280b2e4a18618b908076d71579acd041b357c
Victor Duprez <victor@ectorparking.com>
2018-08-31 09:20:30 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+export const METHOD_POST = 'post';
+export const METHOD_GET = 'get';
+export const METHOD_DELETE = 'delete';
+export const METHOD_PATCH = 'patch';
+export const METHOD_PUT = 'put';
+export const METHOD_OPTIONS = 'options';
+
+class SimpleFaker {
+  constructor(port) {
+    this.app = express();
+    this.app.use(bodyParser.json());
+    this.app.listen(
+      port,
+      () => console.log(`Simple Faker started on port ${port}`),
+    );
+  }
+
+  interceptCall = (path, method, handler) => {
+    let resolve = null;
+    const listener = new Promise((r) => {
+      resolve = r;
+    });
+
+    this.app[method](path, (req, res, next) => {
+      resolve(req)
+      handler(req, res, next);
+    });
+
+    return listener;
+  }
+}
+
+export default SimpleFaker;
