be29444053e9016042b7fd0f8ba19c2b4d2dc9da
Victor Duprez <victor@ectorparking.com>
2018-08-31 10:48:09 +0200

    Add tests

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 
-export const METHOD_POST = 'post';
-export const METHOD_GET = 'get';
-export const METHOD_DELETE = 'delete';
-export const METHOD_PATCH = 'patch';
-export const METHOD_PUT = 'put';
-export const METHOD_OPTIONS = 'options';
+export const METHOD_POST = "post";
+export const METHOD_GET = "get";
+export const METHOD_DELETE = "delete";
+export const METHOD_PATCH = "patch";
+export const METHOD_PUT = "put";
+export const METHOD_OPTIONS = "options";
 
 class SimpleFaker {
   constructor(port) {
     this.app = express();
     this.app.use(bodyParser.json());
-    this.app.listen(
-      port,
-      () => console.log(`Simple Faker started on port ${port}`),
+    this.server = this.app.listen(port, () =>
+      console.log(`Simple Faker started on port ${port}`)
     );
   }
 
-  interceptCall = (path, method, handler) => {
+  interceptCall(path, method, handler) {
     let resolve = null;
-    const listener = new Promise((r) => {
+    const listener = new Promise(r => {
       resolve = r;
     });
 
     this.app[method](path, (req, res, next) => {
-      resolve(req)
+      resolve(req);
       handler(req, res, next);
     });
 
     return listener;
+  }
+
+  close() {
+    return new Promise(resolve =>
+      this.server.close(() => {
+        console.log("Simple Faker closed");
+        resolve();
+      })
+    );
   }
 }
 
